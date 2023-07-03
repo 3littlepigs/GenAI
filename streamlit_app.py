e96a012fb8d522f1a8f9a3c089807a930468c3f5
3littlepigs <36431181+3littlepigs@users.noreply.github.com>
2023-07-03 00:30:47 -0400

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+from langchian.llms import OpenAI
+
+st.title('onborading')
+
+openai_api_key = st.sidebar.text_input('OpenAI API Key')
+
+def generate_response(input_test):
+    llm = OpenAI(temperature=0.7, open_api_key=openai_api_key)
+    st.info(llm(input_text))
+
+with st.form('my_form'):
+    text = st.text_area('Enter text:', 'What is the onboarding pacpage?')
+    submitted = st.form_submit_button('Submit')
+    if not openai_api_key.startswith('sk-'):
+      st.warning('Please enter your OpenAI API key!', icon='⚠')
+    if submitted and openai_api_key.startswith('sk-'):
+      generate_response(text)                                                                                                                                                            )
